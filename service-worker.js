0e7fed5c5648bc6d6d5a43a0fdc640f92c0030aa
OS2112 <59011175+OS2112@users.noreply.github.com>
2025-05-06 16:12:14 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function(e) {
+  console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(['index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
